c3c4a2b5d94edbd690eb76e7578ec1635b20a1f1
muchirijane <muchirijane@outlook.com>
2021-08-01 22:43:52 +0300

    Added state hooks to handle user verification.

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,43 +1,82 @@
-import Image from 'next/image';
-import { FaGoogle, FaFacebookF } from 'react-icons/fa';
-import loginImage from '../../public/Girl.svg';
-import { LoginWrapper, LoginContainer, LoginImageWrapper, LoginFormWrapper, FormInputWrapper, ButtonWrapper } from './Login.styles.js';
-export default function Login() {
-	return (
-		<LoginWrapper>
-			<LoginContainer>
-				<LoginImageWrapper>
-					<Image src={loginImage} alt='An illustration of a girl sitting on a chair'/>
-				</LoginImageWrapper>
-				<LoginFormWrapper>
-					<h1>Welcome Back to Budgetfy</h1>
-					<h2>Login to your account</h2>
-					<form>
-						<FormInputWrapper>
-							<label htmlFor="email-address">Enter Your Email address</label>
+import Image from "next/image";
+import { FaGoogle, FaFacebookF } from "react-icons/fa";
+import loginImage from "../../public/Girl.svg";
+import {
+  LoginWrapper,
+  LoginContainer,
+  LoginImageWrapper,
+  LoginFormWrapper,
+  FormInputWrapper,
+  ButtonWrapper,
+} from "./Login.styles.js";
+export default function Login({
+  email,
+  setEmail,
+  password,
+  setPassword,
+  handleLogin,
+  handleSignup,
+  emailError,
+  passwordError,
+  hasAccount,
+  setHasAccount,
+}) {
+  const emailHandler = (e) => {
+    const emailValue = e.target.value;
+    return emailValue;
+  };
+  return (
+    <LoginWrapper>
+      <LoginContainer>
+        <LoginImageWrapper>
+          <Image
+            src={loginImage}
+            alt="An illustration of a girl sitting on a chair"
+          />
+        </LoginImageWrapper>
+        <LoginFormWrapper>
+          <h1>Welcome Back to Budgetfy</h1>
+          <h2>Login to your account</h2>
+          <form>
+            <FormInputWrapper>
+              <label htmlFor="email-address">Enter Your Email address</label>
 
-							<input id="email-address" type="text" name="email-address" />
-						</FormInputWrapper>
-						<FormInputWrapper>
-							<label htmlFor="password"> Enter your password</label>
+              <input
+                id="email-address"
+                type="text"
+                name="email-address"
+                value={email}
+                required
+                setEmail={emailHandler}
+              />
+              <p>{emailError}</p>
+            </FormInputWrapper>
+            <FormInputWrapper>
+              <label htmlFor="password"> Enter your password</label>
 
-							<input id="password" type="password" name="password" />
-						</FormInputWrapper>
-						<ButtonWrapper>
-							<button className="link-animated" type="submit">
-								Login
-							</button>
-						</ButtonWrapper>
-					</form>
-					<div>
-						<p>Or sign in with</p>
-						<div>
-							<FaGoogle />
-							<FaFacebookF />
-						</div>
-					</div>
-				</LoginFormWrapper>
-			</LoginContainer>
-		</LoginWrapper>
-	);
+              <input
+                id="password"
+                type="password"
+                name="password"
+                value={password}
+              />
+              <p>{passwordError}</p>
+            </FormInputWrapper>
+            <ButtonWrapper>
+              <button className="link-animated" type="submit">
+                Login
+              </button>
+            </ButtonWrapper>
+          </form>
+          <div>
+            <p>Or sign in with</p>
+            <div>
+              <FaGoogle />
+              <FaFacebookF />
+            </div>
+          </div>
+        </LoginFormWrapper>
+      </LoginContainer>
+    </LoginWrapper>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,99 @@
-import {useState, useEffect} from 'react'
-import Layout from '../components/Layout/Layout';
-import Login from '../components/Login/Login';
-import fire from '../firebase/firebase'
+import { useState, useEffect } from "react";
+import Layout from "../components/Layout/Layout";
+import Login from "../components/Login/Login";
+import fire from "../firebase/firebase";
 export default function Home() {
-	const [user, setUser] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [emailError, setEmailError] = useState('');
-	const [passwordError, setPasswordError] = useState('')
-	const [hasAccount, setHasAccount] = useState(false);
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
 
-	const handleLogin = () => {
-		fire
-			.auth()
-			.signInWithEmailAndPassword(email, password)
-	}
-	return (
-		<Layout>
-			<Login />
-		</Layout>
-	);
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+  };
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+
+  const handleLogin = () => {
+    clearErrors();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+            setEmailError("Your email address appears to be malformed.");
+            break;
+          case "auth/user-disabled":
+            setEmailError("User with this email has been disabled.");
+            break;
+          case "auth/user-not-found":
+            setEmailError("The user of this email doesn't exist.");
+            break;
+
+          case "auth/wrong-password":
+            setEmailError("Your password is not correct");
+        }
+      });
+  };
+
+  const handleSignup = () => {
+    clearErrors();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+            setEmailError("Your email address is already in use, please login.");
+            break;
+          case "auth/invalid-email":
+            setEmailError("Your email address is invalid.");
+            break;
+          case "auth/weak-password":
+            setPasswordError(
+              "Your password is weak add characters (@$*?!) and numbers"
+            );
+            break;
+        }
+      });
+  };
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        clearInputs();
+      } else {
+        setUser("");
+      }
+    });
+  };
+  useEffect(() => {
+    authListener();
+  }, []);
+
+  return (
+    <Layout>
+      <Login
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleLogin={handleLogin}
+        handleSignup={handleSignup}
+        hasAccount={hasAccount}
+        setHasAccount={setHasAccount}
+        emailError={emailError}
+        passwordError={passwordError}
+      />
+    </Layout>
+  );
 }
